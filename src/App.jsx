8efc46cfdb7f26727aa3fa4b5a488a9cc3bd2614
Ibrohimstilllearning <almasaraniibrohim@gmail.com>
2026-02-02 13:26:49 +0700

    Hero✔

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import DataImage from "./data";
 function App() {
   return (
     <>
-      <div className="hero grid grid-cols-2">
+      <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
             <img
@@ -20,20 +20,20 @@ function App() {
             untuk database. Saya tertarik pada bidang ini berlangsung selama 2
             tahun.
           </p>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center sm:gap-4 gap-2">
             <a
               href="#"
               className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-500 transition"
             >
-              Download CV
+              Download CV <i className="ri-download-line"></i>
             </a>
-            <a href="#">Lihat Proyek</a>
+            <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600 transition ease-in">Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i></a>
           </div>
         </div>
         <img
           src={DataImage.HeroImage}
           alt="Hero Image"
-          className="w-[500] ml-auto"
+          className="w-[500] md:ml-auto"
         />
       </div>
     </>
